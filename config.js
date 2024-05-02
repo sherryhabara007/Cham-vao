1cb0f4f120ad59b8faef1cb90a9ab94b04968e42
BLUE <93502047+sherryhabara007@users.noreply.github.com>
2024-05-03 00:49:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Nam khờ sinh nhật vui vẻ, tuổi mới nhìu ni
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'namkho.jpg';
+const giftImage = 'hot-gỉrl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
